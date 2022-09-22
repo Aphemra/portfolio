43b84e40ede8acb062c5e350e3603c883c5cfe96
aphemra <aphemra@gmail.com>
2022-09-22 11:45:55 -0400

    Added Theming * Light and dark mode support * Button to switch between them * Basic CSS to showcase it * Saves theme to localstorage with custom hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import useLocalState from "./hooks/useLocalState";
 import Home from "./pages/Home";
 import NoPage from "./pages/NoPage";
 
 function App() {
+	const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+	const [theme, setTheme] = useLocalState("theme", defaultDark ? "dark" : "light");
+
+	function switchTheme() {
+		const newTheme = theme === "light" ? "dark" : "light";
+		setTheme(newTheme);
+	}
+
 	return (
-		<BrowserRouter basename={"/portfolio/"}>
+		<div className="app" data-theme={theme}>
+			<nav className="navbar">
+				<ul>
+					<li>
+						<button onClick={switchTheme}>Switch to {theme === "light" ? "Dark" : "Light"} Theme</button>
+					</li>
+				</ul>
+			</nav>
 			<Routes>
 				<Route index element={<Home />} />
 				<Route path="*" element={<NoPage />} />
 			</Routes>
-		</BrowserRouter>
+		</div>
 	);
 }
 
