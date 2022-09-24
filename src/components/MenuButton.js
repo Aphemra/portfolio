99fa0a5ee611255c191e52d6f177bb064b2875ea
aphemra <aphemra@gmail.com>
2022-09-24 12:06:00 -0400

    Made the navbar responsive * Added a MenuButton component for the hamburger menu button for mobile screen sizes * Added 3 layers of media query for responsiveness * First is full screen, standard size * Second is smaller standard navbar * Third is a new side slide-in navbar on menu button press

---
+++ src/components/MenuButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+function MenuButton({ openMenu }) {
+	return (
+		<div className="side-navbar-button">
+			<button className="menu-button" onClick={openMenu}>
+				<FontAwesomeIcon className="menu-icon" icon={faBars} />
+			</button>
+		</div>
+	);
+}
+
+export default MenuButton;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,39 +1,90 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faMoon } from "@fortawesome/free-regular-svg-icons";
+import MenuButton from "./MenuButton";
 
 function NavBar({ theme, switchTheme }) {
 	const resumePDFLink = "https://drive.google.com/file/d/1AnbkQUZR-gCo5-qJsqJzUFEgjGvexkA9/view?usp=sharing";
 
+	function toggleMenu() {
+		const sideNavElementClasses = document.getElementsByClassName("side-navbar")[0].className;
+		document.getElementsByClassName("side-navbar")[0].className =
+			sideNavElementClasses === "side-navbar hide" ? "side-navbar" : "side-navbar hide";
+		document.getElementsByClassName("modal-lock")[0].className =
+			sideNavElementClasses === "side-navbar hide" ? "modal-lock" : "modal-lock hide";
+	}
+
 	return (
-		<nav className="navbar">
-			<ul className="navbar-links">
-				<li>
-					<a href="#home">Home</a>
-				</li>
-				<li>
-					<a href="#about">About</a>
-				</li>
-				<li>
-					<a href="#portfolio">Portfolio</a>
-				</li>
-				<li>
-					<a href="#contact">Contact</a>
-				</li>
-				<li>
-					<button className="resume-download">
-						<a target="_blank" href={resumePDFLink} rel="noreferrer">
-							Resume
+		<>
+			<MenuButton openMenu={toggleMenu} />
+			<div onClick={toggleMenu} className="modal-lock hide"></div>
+			<nav className="side-navbar">
+				<ul className="side-navbar-links-top">
+					<li>
+						<button className="resume-download">
+							<a target="_blank" href={resumePDFLink} rel="noreferrer">
+								Resume
+							</a>
+						</button>
+					</li>
+					<li>
+						<button className="theme-button" onClick={switchTheme}>
+							<FontAwesomeIcon className="theme-icon" icon={theme === "light" ? faMoon : faSun} />
+						</button>
+					</li>
+				</ul>
+				<ul className="side-navbar-links">
+					<li>
+						<a onClick={toggleMenu} href="#home">
+							Home
 						</a>
-					</button>
-				</li>
-				<li>
-					<button className="theme-button" onClick={switchTheme}>
-						<FontAwesomeIcon className="theme-icon" icon={theme === "light" ? faMoon : faSun} />
-					</button>
-				</li>
-			</ul>
-		</nav>
+					</li>
+					<li>
+						<a onClick={toggleMenu} href="#about">
+							About
+						</a>
+					</li>
+					<li>
+						<a onClick={toggleMenu} href="#portfolio">
+							Portfolio
+						</a>
+					</li>
+					<li>
+						<a onClick={toggleMenu} href="#contact">
+							Contact
+						</a>
+					</li>
+				</ul>
+			</nav>
+			<nav className="navbar">
+				<ul className="navbar-links">
+					<li>
+						<a href="#home">Home</a>
+					</li>
+					<li>
+						<a href="#about">About</a>
+					</li>
+					<li>
+						<a href="#portfolio">Portfolio</a>
+					</li>
+					<li>
+						<a href="#contact">Contact</a>
+					</li>
+					<li>
+						<button className="resume-download">
+							<a target="_blank" href={resumePDFLink} rel="noreferrer">
+								Resume
+							</a>
+						</button>
+					</li>
+					<li>
+						<button className="theme-button" onClick={switchTheme}>
+							<FontAwesomeIcon className="theme-icon" icon={theme === "light" ? faMoon : faSun} />
+						</button>
+					</li>
+				</ul>
+			</nav>
+		</>
 	);
 }
 
